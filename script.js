8673b666bef1b1ac57ba0c95bf92b9b8a457cdc5
Rajat <kaushikrajat777.com>
2023-01-27 12:58:53 +0530

    set localstorage

--- script.js
+++ script.js
@@ -1,55 +1,99 @@
 
-let userNameError=document.getElementById('userNameError');
-let userPassError=document.getElementById('userPasswordError')
-function checkPasswordValidity(password){
+let userNameError = document.getElementById('userNameError');
+let userPassError = document.getElementById('userPasswordError')
+let formContainer = document.getElementById('formContainer')
+let homeDetails = document.getElementById('homeDetails');
+function checkPasswordValidity(password) {
 
     const isContainsUppercase = /^(?=.*[A-Z])/;
     const isContainsLowercase = /^(?=.*[a-z])/;
     const isContainsNumber = /^(?=.*[0-9])/;
     const isContainsSymbol =
-      /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
+        /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
     const isValidLength = /^.{8,16}$/;
-    userPassError.style.display="block";
-    if(password===''){
-        userPassError.innerHTML="Please enter your password!!!"
+    userPassError.style.display = "block";
+    if (password === '') {
+        userPassError.innerHTML = "Please enter your password!!!"
     }
-    else{
+    else {
         if (!isContainsUppercase.test(password)) {
-            userPassError.innerHTML= "Password must have at least one Uppercase Character.";
-          }
-      
-      
-          else if (!isContainsLowercase.test(password)) {
-            userPassError.innerHTML=  "Password must have at least one Lowercase Character.";
-          }
-          else if (!isContainsNumber.test(password)) {
-              userPassError.innerHTML=   "Password must contain at least one Digit.";
-          }
-      
-          else if (!isContainsSymbol.test(password)) {
-              userPassError.innerHTML=   "Password must contain at least one Special Symbol.";
-          }
-          else if (!isValidLength.test(password)) {
-              userPassError.innerHTML=   "Password must be 8-16 Characters Long.";
-          }
-          else{
-              userPassError.style.display="none";
-          }
+            userPassError.innerHTML = "Password must have at least one Uppercase Character.";
+        }
+
+        else if (!isContainsLowercase.test(password)) {
+            userPassError.innerHTML = "Password must have at least one Lowercase Character.";
+        }
+        else if (!isContainsNumber.test(password)) {
+            userPassError.innerHTML = "Password must contain at least one Digit.";
+        }
+
+        else if (!isContainsSymbol.test(password)) {
+            userPassError.innerHTML = "Password must contain at least one Special Symbol.";
+        }
+        else if (!isValidLength.test(password)) {
+            userPassError.innerHTML = "Password must be 8-16 Characters Long.";
+        }
+        else {
+            userPassError.style.display = "none";
+            return true;
+        }
     }
-   
+    return false;
+
+}
+
+function checkUserNameDetails(userName) {
+    const isContainsSymbol =
+        /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
+        userNameError.style.display = 'block';
+    if (userName === '') {
+        userNameError.innerHTML = "Please give valid username!!!";
+        userNameError.style.display = 'block';
+        return false;
+    }
+    else {
+
+        if (isContainsSymbol.test(userName)) {
+            userNameError.innerHTML = "UserName should not  Special Symbol.";
+        }
+        else {
+
+            userNameError.style.display = 'none';
+            return true;
+        }
+        return false;
+    }
+}
+
+function showHomeDetails() {
+    let userNmaeDisplay = localStorage.getItem("UserName");
+    console.log(userNmaeDisplay);
+    if (userNmaeDisplay !== null) {
+        formContainer.style.display = "none";
+        document.getElementById('userNameDisplay').innerHTML = userNmaeDisplay;
+        homeDetails.style.display = "block";
+
+    }
+    else {
+        formContainer.style.display = "block";
+    }
 }
 
 
-function checkLoginDetails(){
-    let userName=document.getElementById('userName').value;
-    let userpassword=document.getElementById('userPassword').value;
+function checkLoginDetails() {
+    let userName = document.getElementById('userName').value;
+    let userpassword = document.getElementById('userPassword').value;
 
-    if(userName===''){
-        userNameError.innerHTML="Please give valid username!!!";
-        userNameError.style.display='block';
+
+    if (checkUserNameDetails(userName) && checkPasswordValidity(userpassword)) {
+        localStorage.setItem("UserName", userName);
+        showHomeDetails();
     }
-    else{
-        userNameError.style.display='none';
-    }
-    checkPasswordValidity(userpassword);
 }
+
+function logoutFromHomePage() {
+    localStorage.clear();
+    location.reload();
+}
+
+showHomeDetails();
